baf968968c8c8995d37a1eeed879afa075c23552
Brayden <banjosquash@gmail.com>
2022-09-26 11:33:00 -0400

    deployed to heroku

--- server.js
+++ server.js
@@ -2,8 +2,6 @@
 const PORT = process.env.PORT || 3001;
 const express = require('express');
 const app = express();
-// const fs = require('fs');
-// const path = require('path');
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
