5b96728d147f5d5572fd10999432d91a44b334af
Brayden <banjosquash@gmail.com>
2022-09-26 13:02:23 -0400

    added and got all routes working , am now able to successfully add and remove notes

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 
-const { notes } = require('../../develop/db/db');
+const { notes } = require('../../db/db.json');
 
-const { createNewNote } = require('../../lib/noteFunction');
+const { createNewNote, deleteNote  } = require('../../lib/noteFunction');
 
 router.get('/notes', (req, res) => {
     let savedNotes = notes;
@@ -13,6 +13,11 @@ router.post('/notes', (req, res) => {
     req.body.id = notes.length.toString();
     let note = createNewNote(req.body, notes);
     res.json(note);
+});
+
+router.delete('/notes/:id', (req, res) => {
+    deleteNote(notes, req.params.id);
+    res.json(notes);
 });
 
 module.exports= router;
